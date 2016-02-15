85e94adfa5eadb117b3a9279c9a6179e351727b7
Death15 <arash.official15@gmail.com>
2016-02-15 09:50:41 -0800

    Updated For Sending massage to vip

--- plugins/S2A.lua
+++ plugins/S2A.lua
@@ -14,12 +14,26 @@ local function run(msg, matches)
 				send_large_msg(receiver, response)
 			end
 		end
-	end
+end
+
+	if matches[1] == 's2avip' then
+		if is_sudo(msg) then -- Only sudo !
+			local data = load_data(_config.moderation.data)
+			local vips = 'vips'
+			local response = matches[2]
+			for k,v in pairs(data[tostring(vips)]) do
+				chat_id =  v
+				local receiver = 'chat#id'..chat_id
+				send_large_msg(receiver, response)
+			end
+		end
+		
 end
 return {
   patterns = {
     "^[!/]([Ss]2[Aa]) +(.+)$",
-    "^[!/](send) (%d+) (.*)$"
+    "^[!/](send) (%d+) (.*)$",
+    "^[!/]([Ss]2[Aa]vip) +(.+)$"
   },
   run = run
 }
