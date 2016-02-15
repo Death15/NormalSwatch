11805f7840862f0a965c55f65e1dad1b6805bd15
Death15 <arash.official15@gmail.com>
2016-02-15 10:37:32 -0800

    Create Auto_Leave.lua

---
+++ plugins/Auto_Leave.lua
@@ -0,0 +1,20 @@
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg)
+  if msg.service and msg.action.type == 'chat_add_user' then
+    local data = load_data(_config.moderation.data)
+    if not data[tostring(msg.to.id)] then
+      return "User Who Invited Me has been blocked"
+      chat_del_user('chat#id'..msg.to.id, 'user#id'..167946378, callback, false)
+      block_user("user#id"..msg.from.id,ok_cb,false)
+  end
+end
+
+return {
+  description = "Kicking ourself (bot) from unmanaged groups.",
+  usage = "Plugin For Kiling add with id ....",
+  patterns = {
+    "^!!tgservice
